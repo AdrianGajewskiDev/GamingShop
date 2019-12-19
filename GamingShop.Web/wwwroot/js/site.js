6ea19682a707414d5e101c4d600601e7d5bf7839
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2019-12-19 14:30:13 +0100

    Added a data layer and DbContext class

--- GamingShop.Web/wwwroot/js/site.js
+++ GamingShop.Web/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const btn = document.querySelector(".hamburger-menu");
+const menu = document.querySelector(".nav-links");
+const container = document.querySelector(".nav-links-container");
+
+const showSubMenuBtn = document.querySelector(".showSubMenu");
+const submenu = document.querySelector(".nav-link-submenu");
+
+btn.addEventListener("click", () => {
+    menu.classList.toggle("display");
+    container.classList.toggle("display-flex");
+    console.log("w");
+});
+
+showSubMenuBtn.addEventListener("click", () => {
+    submenu.classList.toggle("display-submenu");
+});
