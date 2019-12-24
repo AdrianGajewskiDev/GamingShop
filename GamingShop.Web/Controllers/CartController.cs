c39fa726e70c0d6a96153932da7a89092c5003ec
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2019-12-24 23:06:14 +0100

    user can remove items from cart

--- GamingShop.Web/Controllers/CartController.cs
+++ GamingShop.Web/Controllers/CartController.cs
@@ -1,20 +1,25 @@
-﻿using GamingShop.Service;
+﻿using GamingShop.Data.Models;
+using GamingShop.Service;
 using GamingShop.Web.Models;
 using GamingShop.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace GamingShop.Web.Controllers
 {
     public class CartController : Controller
     {
         private readonly ICart _cartService;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public CartController(ICart cart)
+
+        public CartController(ICart cart, UserManager<ApplicationUser> manager)
         {
             _cartService = cart;
+            _userManager = manager;
         }
 
         public IActionResult Index(int id)
@@ -25,7 +30,8 @@ namespace GamingShop.Web.Controllers
                 Price = game.Price,
                 Producent = game.Producent,
                 Title = game.Title,
-                ImageUrl = game.ImageUrl
+                ImageUrl = game.ImageUrl,
+                ID = game.ID
             });
 
             var model = new CartIndexModel
@@ -36,6 +42,18 @@ namespace GamingShop.Web.Controllers
 
 
             return View(model);
+        }
+
+        public async Task<IActionResult> RemoveFromCart(int id)
+        {
+
+            var user = await _userManager.GetUserAsync(User);
+
+            var game = _cartService.GetGames(user.CartID).Where(g => g.ID == id).First();
+
+            _cartService.RemoveFormCart(user.CartID, game);
+
+            return RedirectToAction("Index", new { id = user.CartID }) ;
         }
 
         private decimal CalculateTotalPrice(IEnumerable<GameIndexViewModel> games)
