68c7cd1384810366dd6a7c58c74a709ac3039a33
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2019-12-29 18:34:57 +0100

    test

---
+++ GamingShop.Data/MyData/EmailCredentials.cs
@@ -0,0 +1,9 @@
+﻿namespace GamingShop.Data.MyData
+{
+    public static class EmailCredentials
+    {
+        public static string Email = "adrian.gajewskiDev@gmail.com";
+        public static string Password = "Lakiernik2345";
+
+    }
+}
