6dacb0247321ade29174280a91d7a66d1aefe935
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2019-12-29 18:29:47 +0100

    deleted email credentials file

--- GamingShop.Data/MyData/EmailCredentials.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GamingShop.Data.MyData
-{
-    public static class EmailCredentials
-    {
-        public static string Email = "adrian.gajewskiDev@gmail.com";
-        public static string Password = "Lakiernik2345";
-    }
-}
