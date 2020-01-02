962231d0e028ec6636cc8d31e830562d44e92114
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2020-01-02 20:58:56 +0100

    add categories controller

---
+++ GamingShop.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace GamingShop.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
